584e9c86f3445a64be6361dcc8e0e2cbd862f0bd
aetherdies <157361903+aetherdies@users.noreply.github.com>
2024-10-15 16:27:45 +0100

    git init

---
+++ app/Http/Controllers/HorrorMovieController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Horror_Movie;
+use Illuminate\Http\Request;
+
+class HorrorMovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Horror_Movie $horror_Movie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Horror_Movie $horror_Movie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Horror_Movie $horror_Movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Horror_Movie $horror_Movie)
+    {
+        //
+    }
+}
